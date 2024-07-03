29d1f588a7c7ce4b80eb0eedba988caee7f9ef5e
Recep Can Saraç <113214739+RecepCanSarac@users.noreply.github.com>
2024-07-03 16:29:05 +0300

    Player Controller and animation added

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //public SOPlayer playerData;
+
+    private Rigidbody2D rb;
+    private Animator _animator;
+    Vector2 movement;
+    float _speed = 5;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //_speed = playerData.Speed;
+        _animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        _animator.SetFloat("Horizontal", movement.x);
+        _animator.SetFloat("Vertical", movement.y);
+        _animator.SetFloat("speed", movement.sqrMagnitude);
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * _speed * Time.fixedDeltaTime);
+    }
+
+}
